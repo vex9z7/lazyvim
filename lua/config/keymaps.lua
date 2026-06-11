775f04a7028514e3c70a2c42e91320e72d6d635b
vex9z7 <face.tubas0p@icloud.com>
2026-06-11 02:44:55 +0000

    Add terminal escape keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Leave terminal mode with a discoverable escape sequence.
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
