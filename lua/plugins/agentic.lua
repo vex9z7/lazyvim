931ea1c08e0ff175e0162fb5fb743a1df516d9f6
vex9z7 <face.tubas0p@icloud.com>
2026-08-01 19:46:04 +0000

    feat(ai): add agentic acp providers

---
+++ lua/plugins/agentic.lua
@@ -0,0 +1,81 @@
+return {
+  {
+    "carlos-algms/agentic.nvim",
+    keys = {
+      {
+        "<leader>aa",
+        function()
+          require("agentic").toggle()
+        end,
+        mode = { "n", "v" },
+        desc = "Toggle Agentic chat",
+      },
+      {
+        "<leader>aA",
+        function()
+          require("agentic").add_selection_or_file_to_context()
+        end,
+        mode = { "n", "v" },
+        desc = "Add file/selection to Agentic",
+      },
+      {
+        "<leader>an",
+        function()
+          require("agentic").new_session()
+        end,
+        mode = { "n", "v" },
+        desc = "New Agentic session",
+      },
+      {
+        "<leader>ar",
+        function()
+          require("agentic").restore_session()
+        end,
+        mode = { "n", "v" },
+        desc = "Restore Agentic session",
+      },
+      {
+        "<leader>ad",
+        function()
+          require("agentic").add_current_line_diagnostics()
+        end,
+        mode = "n",
+        desc = "Add line diagnostics to Agentic",
+      },
+      {
+        "<leader>aD",
+        function()
+          require("agentic").add_buffer_diagnostics()
+        end,
+        mode = "n",
+        desc = "Add buffer diagnostics to Agentic",
+      },
+    },
+    opts = {
+      provider = "pi-acp",
+      acp_providers = {
+        ["pi-acp"] = {
+          name = "Pi ACP",
+          command = "pi-acp",
+          env = {
+            PI_ACP_PI_COMMAND = vim.fn.stdpath "config" .. "/bin/pi-agentic",
+          },
+        },
+        ["opencode-acp"] = {
+          name = "OpenCode ACP",
+          command = vim.fn.stdpath "config" .. "/bin/opencode-agentic",
+          args = { "acp" },
+          env = {},
+        },
+      },
+      diff_preview = {
+        enabled = true,
+        layout = "inline",
+      },
+      windows = {
+        position = "right",
+        width = "38%",
+      },
+    },
+  },
+}
