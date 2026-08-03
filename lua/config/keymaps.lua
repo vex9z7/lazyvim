aa444850feccd1ecd8556ecc0983c18a5a741d93
vex9z7 <face.tubas0p@icloud.com>
2026-08-03 08:10:55 +0000

    fix: remove Alt line move keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Avoid terminal Alt-key encoding turning a quick Escape followed by j/k into line moves.
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+
 Snacks.toggle({
   name = "Git Diff Overlay",
   get = function()
