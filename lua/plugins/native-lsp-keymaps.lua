1d99bb682a89cf480e967996326285bdbea57af7
vex9z7 <face.tubas0p@icloud.com>
2026-08-03 13:21:52 +0000

    chore: remove duplicate LSP rename keymap

--- lua/plugins/native-lsp-keymaps.lua
+++ lua/plugins/native-lsp-keymaps.lua
@@ -2,10 +2,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      for index, key in ipairs(opts.servers["*"].keys or {}) do
-        if key[1] == "gr" then
+      for index = #(opts.servers["*"].keys or {}), 1, -1 do
+        local key = opts.servers["*"].keys[index]
+        if key[1] == "gr" or key[1] == "<leader>cr" then
           table.remove(opts.servers["*"].keys, index)
-          break
         end
       end
     end,
