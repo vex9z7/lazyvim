0fcfd82cf93ee0435bdef482f875b5ec8d84d5f1
vex9z7 <face.tubas0p@icloud.com>
2026-08-05 07:28:21 +0000

    fix: stage untracked current files

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,7 +5,21 @@ return {
       {
         "<leader>ga",
         function()
-          require("gitsigns").stage_buffer()
+          vim.cmd.update()
+          local file = vim.api.nvim_buf_get_name(0)
+          local root = vim.fs.root(file, { ".git" })
+          if not root then
+            vim.notify("Current file is not in a Git repository", vim.log.levels.WARN)
+            return
+          end
+          local result = vim
+            .system({ "git", "-C", root, "add", "--", vim.fs.relpath(root, file) }, { text = true })
+            :wait()
+          if result.code ~= 0 then
+            vim.notify(result.stderr, vim.log.levels.ERROR)
+            return
+          end
+          require("gitsigns").refresh()
         end,
         desc = "Git Add Current File",
       },
