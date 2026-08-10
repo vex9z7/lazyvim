5eb82a00591ee2d5eaf39e87348cab62e1e7ea96
vex9z7 <face.tubas0p@icloud.com>
2026-08-10 13:38:47 +0000

    Revert "fix: omit global completion snippets"
    This reverts commit e427bed81aa0a9c485ef568e0705bd090024ec5a.

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -51,17 +51,6 @@ return {
       signature = {
         enabled = true,
       },
-      sources = {
-        providers = {
-          snippets = {
-            opts = {
-              -- Keep language snippets, but not friendly-snippets' catch-all
-              -- templates (for example, the copyright boilerplate in Oil).
-              global_snippets = {},
-            },
-          },
-        },
-      },
     },
   },
 }
