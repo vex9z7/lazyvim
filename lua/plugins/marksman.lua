42bf23086af2d7ab1fc4436dbd01e7858db042f8
vex9z7 <face.tubas0p@icloud.com>
2026-08-25 05:58:07 +0000

    Revert "fix: fall back to native Markdown links"
    This reverts commit ca1e92d062e2ceffa07fc2fac2399e0478cd2aa0.

--- lua/plugins/marksman.lua
+++ lua/plugins/marksman.lua
@@ -3,23 +3,8 @@ return {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
       opts.servers = opts.servers or {}
-      opts.servers.marksman = {
-        -- Marksman uses the repository root for cross-file Markdown links.
-        on_attach = function(client, buffer)
-          vim.keymap.set("n", "gd", function()
-            local params = vim.lsp.util.make_position_params(0, client.offset_encoding)
-            client:request("textDocument/definition", params, function(err, result)
-              if err or not result or vim.tbl_isempty(result) then
-                vim.schedule(function()
-                  vim.cmd.normal { args = { "gf" }, bang = true }
-                end)
-                return
-              end
-              vim.lsp.util.show_document(result, client.offset_encoding)
-            end, buffer)
-          end, { buffer = buffer, desc = "Goto Markdown Link" })
-        end,
-      }
+      -- Marksman uses the repository root for cross-file Markdown links.
+      opts.servers.marksman = {}
     end,
   },
 }
